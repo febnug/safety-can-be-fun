44258283b7b68180bad84d404cc73c375dfbb186
Febriyanto Nugroho <vmvm1071@gmail.com>
2022-01-21 13:38:58 +0700

    I'm stuck in the middle (⌣̩̩́_⌣̩̩̀)
    After a long time playing at low-level, I decided to take a break for a while somehow or maybe forever.
    
    Sincerely yours,
    
    febnug (Febriyanto Nugroho)

--- pm.asm
+++ pm.asm
@@ -1,71 +1,116 @@
+; Contoh x86 protected mode
+; (c) Febriyanto Nugroho - 2022
+
+[bits 16]
 [org 0x7c00]
-[BITS 16]
+
+    jmp 0:kernel_start
+
+gdt_start:
+
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+gdt_code:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start
+    dd gdt_start
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-__start:
-	jmp	short _start
-	nop
-	times 33 db 0
-_start:
-	jmp	0x0:start
-start:
-	cli
-	xor	ax, ax
-	mov	ds, ax
-	mov	es, ax
+print:
+    pusha
+    mov ah, 14
+    mov bh, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+.done:
+    popa
+    ret
 
-	xor	ax, ax
-	mov	ss, ax
-	mov	sp, 0x7c00
-	sti
+uzenet16 db 'uzenet16', 0
+uzenet32 db 'uzenet32', 0
 
-.load_protected:
-	cli
-	lgdt	[gdt_descriptor]
-	; Whee, we are entering the protected mode!
-	mov	eax, cr0
-	or	eax, 1
-	mov	cr0, eax
-	jmp	CODE_SEG:load32
+kernel_start:
+    mov ax, 0
+    mov ss, ax
+    mov sp, 0xFFFC
 
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-; GDT
-gdt_start:
+    mov si, uzenet16
+    call print
 
-gdt_null:
-	dd	0x0
-	dd	0x0
-	; Offset 0x8
-gdt_code:			; CS should point to this label.
-	dw	0xffff		; Segment limit first 0-15 bit.
-	dw	0x0		; Base first 0-15 bits.
-	db	0x0		; Base 16-23 bits.
-	db	0x9a		; Access byte.
-	db	11001111b	; High 4 bit flags and low 4 bit flags.
-	db	0x0		; Base 24-31 bits.
-	; Offset 0x10
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:b32
 
-gdt_data:			; DS, SS, ES, FS, GS
-	dw	0xffff		; Segment limit first 0-15 bit.
-	dw	0x0		; Base first 0-15 bits.
-	db	0x0		; Base 16-23 bits.
-	db	0x92		; Access byte.
-	db	11001111b	; High 4 bit flags and low 4 bit flags.
-	db	0x0		; Base 24-31 bits.
-gdt_end:
+[bits 32]
 
-gdt_descriptor:
-	dw	gdt_end - gdt_start - 1
-	dd	gdt_start
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
 
-[BITS 32]
-load32:
-  xchg  eax, eax ; dunno, untested
-  jmp $
+print32:
+    pusha
+    mov edx, VIDEO_MEMORY
+.loop:
+    mov al, [ebx]
+    mov ah, WHITE_ON_BLACK
+    cmp al, 0
+    je .done
+    mov [edx], ax
+    add ebx, 1
+    add edx, 2
+    jmp .loop
+.done:
+    popa
+    ret
 
+b32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-end_of_code:
-	times 510 - ($ - $$) db 0
-	dw 0xaa55
+    mov ebp, 0x2000
+    mov esp, ebp
+
+    mov ebx, uzenet32
+    call print32
+
+    jmp $
+
+[SECTION signature start=0x7dfe]
+dw 0AA55h
